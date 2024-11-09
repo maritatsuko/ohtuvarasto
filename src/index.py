ff8df3b11777155d3f17797ed88150c937074c83
Mari Lehtikari <mari.lehtikari@helsinki.fi>
2024-11-09 20:21:30 +0200

    korjataan sääntörike

--- src/index.py
+++ src/index.py
@@ -47,7 +47,7 @@ def main():
     #print(f"saatiin {saatiin}")
     #print(f"Olutvarasto: {olutta}")
 
-    print(f"Mehuvarasto: {mehua}")
+    #print(f"Mehuvarasto: {mehua}")
     print("mehua.otaVarastosta(-32.9)")
     saatiin = mehua.ota_varastosta(-32.9)
     print(f"saatiin {saatiin}")
